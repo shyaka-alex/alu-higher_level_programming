7705ad999a012e304717c0eb8527d2533e1292de
shyaka-alex <your@email.com>
2026-06-20 14:42:07 -0700

    Fix tasks 6 and 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language with Python"
-print(str[40:66] + str[76:])
+print(str[39:] [2:] [:34] + str[-11:] [-6:])
+EOFcat > /alu-higher_level_programming/python-hello_world/8-concat_edges.py << 'EOF'
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming language with Python"
+print(str[39:] [2:] [:34] + str[-11:] [-6:])
