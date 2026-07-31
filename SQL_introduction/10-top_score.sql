478a3645a66d8705c199685505bee98106f41d3c
shyaka-alex <your@email.com>
2026-07-31 14:09:54 -0700

    Add SQL introduction tasks

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- List all records ordered by score descending
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,2 @@
+-- List number of records per score
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
