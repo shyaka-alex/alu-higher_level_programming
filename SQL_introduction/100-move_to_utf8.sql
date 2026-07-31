3b3a14dd8f7fc1e51acafffc91a5f4dfc5161a5e
shyaka-alex <your@email.com>
2026-07-31 14:12:28 -0700

    Add advanced SQL introduction tasks

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- Convert hbtn_0c_0 database, first_table and name field to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CHANGE name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
