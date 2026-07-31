faecb47f171d45f04b80d1d852931f7868f67145
shyaka-alex <your@email.com>
2026-07-31 14:32:14 -0700

    Add advanced SQL more queries tasks

---
+++ SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,9 @@
+-- List all genres not linked to Dexter
+SELECT name FROM tv_genres
+WHERE name NOT IN (
+    SELECT tv_genres.name FROM tv_genres
+    JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+    WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY name ASC;
---
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,9 @@
+-- List all shows without Comedy genre
+SELECT title FROM tv_shows
+WHERE title NOT IN (
+    SELECT tv_shows.title FROM tv_shows
+    JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Comedy'
+)
+ORDER BY title ASC;
